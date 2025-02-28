357241b52857527db38c03fe20c82620b0f5f788
RidaFrida <ismailovafafa@gmail.com>
2025-02-28 12:01:29 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    Telegram.WebApp.expand(); // Расширяет веб-приложение на весь экран
-});
-
-function sendRequest(action) {
-    const tg = Telegram.WebApp;
-    tg.sendData(action); // Отправка данных боту
-    tg.close(); // Закрытие Web App после отправки
-}
